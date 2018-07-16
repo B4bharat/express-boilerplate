c35177e76e1b1fc43182723b82caad9669938640
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2018-07-16 14:56:44 +0530

    added detailed logging through winston in development environment

--- src/config/express.js
+++ src/config/express.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const compress = require('compression');
 const methodOverride = require('method-override');
 const cors = require('cors');
 const helmet = require('helmet');
 const passport = require('passport');
+const winston = require('winston');
+const expressWinston = require('express-winston');
 const routes = require('../api/routes/v1');
-const { logs } = require('./vars');
 const strategies = require('./passport');
 const error = require('../api/middlewares/error');
+const { env } = require('./vars');
 
 /**
-* Express instance
-* @public
-*/
+ * Express instance
+ * @public
+ */
 const app = express();
-
-// request logging. dev: console | production: file
-app.use(morgan(logs));
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
@@ -42,6 +40,41 @@ app.use(passport.initialize());
 passport.use('jwt', strategies.jwt);
 passport.use('facebook', strategies.facebook);
 passport.use('google', strategies.google);
+
+// enable detailed API logging in dev env
+if (env === 'development') {
+  expressWinston.requestWhitelist.push('body');
+  expressWinston.responseWhitelist.push('body');
+  app.use(expressWinston.logger({
+    statusLevels: false, // default value
+    level(req, res) {
+      let level = '';
+      if (res.statusCode >= 100) {
+        level = 'info';
+      }
+      if (res.statusCode >= 400) {
+        level = 'warn';
+      }
+      if (res.statusCode >= 500) {
+        level = 'error';
+      }
+      // Ops is worried about hacking attempts so make Unauthorized and Forbidden critical
+      if (res.statusCode === 401 || res.statusCode === 403) {
+        level = 'critical';
+      }
+      return level;
+    },
+    transports: [
+      new winston.transports.Console({
+        json: true,
+        colorize: true,
+      }),
+    ],
+    meta: true, // optional: log meta data about request (defaults to true)
+    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+    colorStatus: true, // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+  }));
+}
 
 // mount api v1 routes
 app.use('/v1', routes);
