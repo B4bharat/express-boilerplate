636ae30a959c35ac4086f2e8a545c1856a7142f7
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2018-07-18 18:55:40 +0530

    fixed the linting npm scripts

--- src/app/middlewares/error.js
+++ src/app/middlewares/error.js
@@ -8,20 +8,20 @@ const { env } = require('../../config/vars');
  * @public
  */
 const handler = (err, req, res, next) => {
-  const response = {
-    code: err.status,
-    message: err.message || httpStatus[err.status],
-    errors: err.errors,
-    stack: err.stack,
-  };
+	const response = {
+		code: err.status,
+		message: err.message || httpStatus[err.status],
+		errors: err.errors,
+		stack: err.stack,
+	};
 
-  if (env !== 'development') {
-    delete response.stack;
-  }
+	if (env !== 'development') {
+		delete response.stack;
+	}
 
-  res.status(err.status);
-  res.json(response);
-  res.end();
+	res.status(err.status);
+	res.json(response);
+	res.end();
 };
 
 exports.handler = handler;
@@ -31,24 +31,24 @@ exports.handler = handler;
  * @public
  */
 exports.converter = (err, req, res, next) => {
-  let convertedError = err;
+	let convertedError = err;
 
-  if (err instanceof expressValidation.ValidationError) {
-    convertedError = new APIError({
-      message: 'Erro de Validação',
-      errors: err.errors,
-      status: err.status,
-      stack: err.stack,
-    });
-  } else if (!(err instanceof APIError)) {
-    convertedError = new APIError({
-      message: err.message,
-      status: err.status,
-      stack: err.stack,
-    });
-  }
+	if (err instanceof expressValidation.ValidationError) {
+		convertedError = new APIError({
+			message: 'Erro de Validação',
+			errors: err.errors,
+			status: err.status,
+			stack: err.stack,
+		});
+	} else if (!(err instanceof APIError)) {
+		convertedError = new APIError({
+			message: err.message,
+			status: err.status,
+			stack: err.stack,
+		});
+	}
 
-  return handler(convertedError, req, res);
+	return handler(convertedError, req, res);
 };
 
 /**
@@ -56,10 +56,10 @@ exports.converter = (err, req, res, next) => {
  * @public
  */
 exports.notFound = (req, res, next) => {
-  const err = new APIError({
-    message: 'Not found',
-    status: httpStatus.NOT_FOUND,
-  });
+	const err = new APIError({
+		message: 'Not found',
+		status: httpStatus.NOT_FOUND,
+	});
 
-  return handler(err, req, res);
+	return handler(err, req, res);
 };
