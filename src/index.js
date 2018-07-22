3bcfb7590d1c6323bbab1ba7c8e9ce03088edca4
Bharat Poptwani <b4bharat.available@gmail.com>
2018-07-22 20:05:37 +0530

    removed the todo list from index.js

--- src/index.js
+++ src/index.js
@@ -7,27 +7,6 @@ const mongoose = require('./config/mongoose');
 // open mongoose connection
 mongoose.connect();
 
-/**
- X* Logging
- X* Routing/Folder Structure
- X* Eslint from shetty's config, prettier and husky[pre-commit hooks]
- X* Express JWT for Authentication
- * mlab
- X* Documentation [Swagger]
- * Node Best Practices Github
- * Security Best Practices - Later
- * Performance Best Practices - Later
- * joi vs Express Validator
- * Configuration Environment
- * Mongoose Wrapper with Singleton Connection - Pratik
- * Error handling middleware
- * Error Logging using Winston
- * Unit Testing - Pratik
- * npm scripts
- * ReadME
- * GraphQL
- */
-
 // listen to requests
 app.listen(port, () => console.info(`server started on port ${port} (${env})`));
 
