92cd13593bd60ae669273e99e21aa3abcc5176d1
Pratik Solim <pratiksolim@pratiksolim.local>
2018-07-23 13:56:16 +0530

    added custom error

---
+++ src/app/utils/customError.js
@@ -0,0 +1,25 @@
+class CustomError extends Error {
+	/**
+	 * Creates an instance of CustomError.
+	 *
+	 * @param {Object} params message and status
+	 * @param {String} params.message message for client side
+	 * @param {Number} params.status status code
+	 * @param {String} extra additional message
+	 * @memberof CustomError
+	 */
+	constructor(params, extra) {
+		super();
+
+		Error.captureStackTrace(this, this.constructor);
+		this.name = this.constructor.name;
+		this.message = params.message;
+		this.status = params.code;
+
+		if (extra) {
+			this.otherMessage = extra;
+		}
+	}
+}
+
+module.exports = CustomError;
